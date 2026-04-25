a89ce4c5d439fcb0c134688081f3cf9d4dbc797d
Aditya <adityachotaliya9299@gmail.com>
2026-04-25 08:26:16 +0000

    feat: finalize VaultAccount state struct

--- programs/solaudd/src/state/vault_state.rs
+++ programs/solaudd/src/state/vault_state.rs
@@ -2,11 +2,11 @@ use anchor_lang::prelude::*;
 
 #[account]
 pub struct VaultAccount {
-    pub merchant:       Pubkey,    // 32
-    pub mint:           Pubkey,    // 32
-    pub merchant_id:    String,    // 4 + 32
-    pub total_received: u64,       // 8
-    pub bump:           u8,        // 1
+    pub merchant:      Pubkey,  // 32
+    pub mint:          Pubkey,  // 32
+    pub merchant_id:   String,  // 4 + 32
+    pub total_received: u64,    // 8
+    pub bump:          u8,      // 1
 }
 
 impl VaultAccount {
