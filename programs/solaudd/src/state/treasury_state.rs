98022aad00632834fa5c0d445ee21510fa7e0520
Aditya <adityachotaliya9299@gmail.com>
2026-04-27 04:56:21 +0000

    feat: finalize TreasuryAccount state struct

--- programs/solaudd/src/state/treasury_state.rs
+++ programs/solaudd/src/state/treasury_state.rs
@@ -2,12 +2,12 @@ use anchor_lang::prelude::*;
 
 #[account]
 pub struct TreasuryAccount {
-    pub authority:    Pubkey,    // 32
-    pub mint:         Pubkey,    // 32
-    pub name:         String,    // 4 + 32
-    pub total_in:     u64,       // 8
-    pub total_out:    u64,       // 8
-    pub bump:         u8,        // 1
+    pub authority: Pubkey,  // 32
+    pub mint:      Pubkey,  // 32
+    pub name:      String,  // 4 + 32
+    pub total_in:  u64,     // 8
+    pub total_out: u64,     // 8
+    pub bump:      u8,      // 1
 }
 
 impl TreasuryAccount {
